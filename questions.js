9d2f5b8ec588649d570af9992573a6142b716d18
Dr Noble Science Tutorial <nobleofbeninkingdom@gmail.com>
2025-05-30 13:26:54 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,10 @@
+const questions = [
+  {
+    question: "Sample question: What is 2 + 2?",
+    options: ["3", "4", "5", "6"],
+    answer: "4",
+    explanation: "2 + 2 = 4"
+  },
+  // Add more questions here...
+];
 

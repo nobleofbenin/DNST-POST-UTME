f1a38c64af5ba8452def6191e85171c117d863a7
Dr Noble Science Tutorial <nobleofbeninkingdom@gmail.com>
2025-05-30 07:36:08 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,11 @@
+// script.js
+
+function startPractice() {
+  // Redirect to practice mode page or initiate practice mode
+  alert("Practice Mode is under development.");
+}
+
+function startExam() {
+  // Redirect to exam mode page or initiate exam mode
+  alert("Exam Mode is under development.");
+}

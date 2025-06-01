8ad1498354ff7180e3069fb7264b4ac44eb00d9f
Dr Noble Science Tutorial <nobleofbeninkingdom@gmail.com>
2025-06-01 02:19:24 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,502 @@
+const questions = [
+  {
+    "question": "The following are stringed instruments except",
+    "options": [
+      "violin",
+      "piano",
+      "guitar",
+      "xylophone"
+    ],
+    "answer": "xylophone"
+  },
+  {
+    "question": "If two 2\u03bcF capacitors are connected in parallel and then in series with a 4\u03bcF capacitor, the net capacitance is",
+    "options": [
+      "6\u03bcF",
+      "4\u03bcF",
+      "2\u03bcF",
+      "0.2\u03bcF"
+    ],
+    "answer": "2\u03bcF"
+  },
+  {
+    "question": "The limiting frictional force on a 4kg body on an inclined plane is 20N. What is the angle of inclination?",
+    "options": [
+      "75\u00b0",
+      "60\u00b0",
+      "45\u00b0",
+      "30\u00b0"
+    ],
+    "answer": "30\u00b0"
+  },
+  {
+    "question": "Which quantity has the same unit as pressure \u00d7 volume?",
+    "options": [
+      "Power",
+      "Acceleration",
+      "Workdone",
+      "Force"
+    ],
+    "answer": "Workdone"
+  },
+  {
+    "question": "Centripetal force F for a body in circular motion is given by",
+    "options": [
+      "m\u03c9\u00b2",
+      "mr\u03c9\u00b2",
+      "m\u03c9\u00b2/r",
+      "m/\u03c9\u00b2r"
+    ],
+    "answer": "m\u03c9\u00b2/r"
+  },
+  {
+    "question": "What is the efficiency of a cell with internal resistance 3\u03a9 supplying current to a 7\u03a9 resistor?",
+    "options": [
+      "70%",
+      "30%",
+      "20%",
+      "10%"
+    ],
+    "answer": "70%"
+  },
+  {
+    "question": "Which of the following compounds is not obtained by polymerization?",
+    "options": [
+      "Plastic",
+      "Polythene",
+      "Petroleum",
+      "Cellulose"
+    ],
+    "answer": "Petroleum"
+  },
+  {
+    "question": "A zinc specimen containing zinc oxide reacts with acid to yield 215cm\u00b3 hydrogen at 17\u00b0C and 770mmHg. % of ZnO in 0.80g sample?",
+    "options": [
+      "15%",
+      "25%",
+      "35%",
+      "45%"
+    ],
+    "answer": "25%"
+  },
+  {
+    "question": "A measure of the degree of disorderliness in a chemical system is known as",
+    "options": [
+      "enthalpy",
+      "entropy",
+      "free energy",
+      "activation energy"
+    ],
+    "answer": "entropy"
+  },
+  {
+    "question": "In the reaction Cr2O7\u00b2\u207b + 6Fe\u00b2\u207a + 14H\u207a \u2192 2Cr\u00b3\u207a + 6Fe\u00b3\u207a + 7H2O, chromium changes from",
+    "options": [
+      "+7 to +3",
+      "+6 to +3",
+      "-6 to +3",
+      "-2 to +6"
+    ],
+    "answer": "+6 to +3"
+  },
+  {
+    "question": "Which of the following metals dissolves in aqueous sodium hydroxide?",
+    "options": [
+      "Calcium",
+      "Copper",
+      "Aluminium",
+      "Iron"
+    ],
+    "answer": "Aluminium"
+  },
+  {
+    "question": "The alloy used in plumbing contains",
+    "options": [
+      "lead and tin",
+      "lead and antimony",
+      "iron and aluminium",
+      "copper and tin"
+    ],
+    "answer": "lead and tin"
+  },
+  {
+    "question": "Deficiency of vitamin B1 (thiamine) leads to",
+    "options": [
+      "goitre",
+      "beri-beri",
+      "scurvy",
+      "pellagra"
+    ],
+    "answer": "beri-beri"
+  },
+  {
+    "question": "Which of the following is not a component of guard cells?",
+    "options": [
+      "Chloroplasts",
+      "Nucleus",
+      "thin inner wall",
+      "rough spike"
+    ],
+    "answer": "rough spike"
+  },
+  {
+    "question": "The enzyme of the glycolytic pathway is located in the",
+    "options": [
+      "mitochondria",
+      "golgi apparatus",
+      "cytoplasm",
+      "Nucleus"
+    ],
+    "answer": "cytoplasm"
+  },
+  {
+    "question": "Light is required in photosynthesis to",
+    "options": [
+      "oxidize water molecules",
+      "split water molecules",
+      "reduce CO\u2082",
+      "fix CO\u2082"
+    ],
+    "answer": "split water molecules"
+  },
+  {
+    "question": "The diploid number of chromosomes in man is",
+    "options": [
+      "46",
+      "23",
+      "48",
+      "26"
+    ],
+    "answer": "46"
+  },
+  {
+    "question": "All are parts of the continental shelf except",
+    "options": [
+      "splash zone",
+      "subtidal zone",
+      "intertidal zone",
+      "benthic zone"
+    ],
+    "answer": "benthic zone"
+  },
+  {
+    "question": "Part of the brain that coordinates involuntary actions like heartbeat is",
+    "options": [
+      "medulla oblongata",
+      "cerebellum",
+      "pineal",
+      "cerebrum"
+    ],
+    "answer": "medulla oblongata"
+  },
+  {
+    "question": "A curved velocity-time graph indicates",
+    "options": [
+      "a uniform acceleration",
+      "a non-uniform acceleration",
+      "a uniform velocity",
+      "a non-uniform velocity"
+    ],
+    "answer": "a non-uniform acceleration"
+  },
+  {
+    "question": "A 1kg ball falls from 3m and rebounds to 2m. Energy loss due to impact is",
+    "options": [
+      "100J",
+      "50J",
+      "10J",
+      "1J"
+    ],
+    "answer": "10J"
+  },
+  {
+    "question": "A projectile reaches max range of 40m. What is its velocity? (g = 10 m/s\u00b2)",
+    "options": [
+      "400 m/s",
+      "80 m/s",
+      "20 m/s",
+      "10 m/s"
+    ],
+    "answer": "20 m/s"
+  },
+  {
+    "question": "A 10kg ball hits a wall at 5 m/s and rebounds. Impulse on the wall is",
+    "options": [
+      "0",
+      "25 Ns",
+      "50 Ns",
+      "100 Ns"
+    ],
+    "answer": "100 Ns"
+  },
+  {
+    "question": "Find velocity of wave from y = 20 sin(60\u03c0t - 0.5\u03c0x)",
+    "options": [
+      "30 m/s",
+      "60 m/s",
+      "80 m/s",
+      "120 m/s"
+    ],
+    "answer": "120 m/s"
+  },
+  {
+    "question": "15 cm\u00b3 of hydrocarbon combusts with 75 cm\u00b3 O\u2082, gives 45 cm\u00b3 CO\u2082. Formula?",
+    "options": [
+      "CH\u2084",
+      "C\u2082H\u2086",
+      "C\u2083H\u2088",
+      "C\u2084H\u2081\u2080"
+    ],
+    "answer": "C\u2083H\u2088"
+  },
+  {
+    "question": "Which reagent differentiates alkanals from alkanones?",
+    "options": [
+      "Hydrogen cyanide",
+      "NaHCO\u2083",
+      "Fehling\u2019s reagent",
+      "2,4-DNP"
+    ],
+    "answer": "Fehling\u2019s reagent"
+  },
+  {
+    "question": "Products of photochlorination of ethanoic acid are",
+    "options": [
+      "CH\u2083COOCl+HCl",
+      "CH\u2083COCl+HOCl",
+      "ClCH\u2082COCl+HCl",
+      "ClCH\u2082COOH+HCl"
+    ],
+    "answer": "ClCH\u2082COOH+HCl"
+  },
+  {
+    "question": "Products of ethyl ethanoate + ammonia are",
+    "options": [
+      "Aminoethane + ethanol",
+      "Aminomethane + propan-1-ol",
+      "Ethanamide + ethanol",
+      "Methanamide + propan-1-ol"
+    ],
+    "answer": "Ethanamide + ethanol"
+  },
+  {
+    "question": "Effervescence occurs when water contacts",
+    "options": [
+      "calcium",
+      "copper",
+      "sulphur",
+      "lead"
+    ],
+    "answer": "calcium"
+  },
+  {
+    "question": "Charring of sugar occurs when it is",
+    "options": [
+      "reduced",
+      "hydrolysed",
+      "dehydrated",
+      "fermented"
+    ],
+    "answer": "dehydrated"
+  },
+  {
+    "question": "Moles of Zn deposited by 3F electricity through ZnSO\u2084 is",
+    "options": [
+      "1.5",
+      "2.3",
+      "3.4",
+      "0.15"
+    ],
+    "answer": "1.5"
+  },
+  {
+    "question": "Rainbow colours are due to",
+    "options": [
+      "polarization",
+      "absorption",
+      "refraction",
+      "interference"
+    ],
+    "answer": "refraction"
+  },
+  {
+    "question": "Coulomb\u2019s law for electric force F between charges q, q at distance x is",
+    "options": [
+      "F = q\u00b2 / 4\u03c0\u03b5\u2080x\u00b2",
+      "F = q / x",
+      "F = x\u00b2 / q\u00b2",
+      "F = -q\u00b2/x"
+    ],
+    "answer": "F = q\u00b2 / 4\u03c0\u03b5\u2080x\u00b2"
+  },
+  {
+    "question": "Eye defect corrected with converging lens is",
+    "options": [
+      "myopia",
+      "hypermetropia",
+      "astigmatism",
+      "night blindness"
+    ],
+    "answer": "hypermetropia"
+  },
+  {
+    "question": "Which arthropod lacks antennae?",
+    "options": [
+      "Spider",
+      "Prawn",
+      "Cockroach",
+      "Millipede"
+    ],
+    "answer": "Spider"
+  },
+  {
+    "question": "Red blood cells in hypertonic solution become",
+    "options": [
+      "crenated",
+      "plasmolysed",
+      "haemolysed",
+      "unaffected"
+    ],
+    "answer": "crenated"
+  },
+  {
+    "question": "OH\u207b concentration in pH 10 solution is",
+    "options": [
+      "10\u207b\u00b2",
+      "10\u207b\u2074",
+      "10\u207b\u00b3",
+      "10\u207b\u00b9"
+    ],
+    "answer": "10\u207b\u2074"
+  },
+  {
+    "question": "Reagent to remove CO\u2082 from gas mix is",
+    "options": [
+      "KOH (aq)",
+      "alkaline pyrogallol",
+      "conc. H\u2082SO\u2084",
+      "calcium oxide"
+    ],
+    "answer": "KOH (aq)"
+  },
+  {
+    "question": "Protozoa includes all except",
+    "options": [
+      "Amoeba",
+      "Chlamydomonas",
+      "Plasmodium",
+      "Trypanosome"
+    ],
+    "answer": "Chlamydomonas"
+  },
+  {
+    "question": "Which plant traps animals with leaves?",
+    "options": [
+      "garlic",
+      "cactus",
+      "sundew",
+      "hibiscus"
+    ],
+    "answer": "sundew"
+  },
+  {
+    "question": "Lukas (O) and Mary (AB) have what chance of child with blood group O?",
+    "options": [
+      "0%",
+      "50%",
+      "70%",
+      "100%"
+    ],
+    "answer": "0%"
+  },
+  {
+    "question": "Air pollutant that causes acid rain is",
+    "options": [
+      "Carbon monoxide",
+      "Sulphur dioxide",
+      "Smoke",
+      "Dust"
+    ],
+    "answer": "Sulphur dioxide"
+  },
+  {
+    "question": "Moving-coil galvanometer works on",
+    "options": [
+      "EM induction",
+      "Magnetic effect of current",
+      "Force on current in field",
+      "Electrochemical effect"
+    ],
+    "answer": "Force on current in field"
+  },
+  {
+    "question": "In a series R-L-C circuit, power consumed is",
+    "options": [
+      "I\u00b2R",
+      "(IX)\u00b2R",
+      "I\u00b2(XL - XC)",
+      "R/XL"
+    ],
+    "answer": "I\u00b2R"
+  },
+  {
+    "question": "All are true of anaerobic respiration EXCEPT",
+    "options": [
+      "by-products are CO\u2082 and H\u2082O",
+      "no oxygen",
+      "alcohol/lactic acid by-products",
+      "occurs in cytoplasm"
+    ],
+    "answer": "by-products are CO\u2082 and H\u2082O"
+  },
+  {
+    "question": "Which life process does not involve meiosis?",
+    "options": [
+      "root tip division",
+      "oogenesis",
+      "spermatogenesis",
+      "pollen formation"
+    ],
+    "answer": "root tip division"
+  },
+  {
+    "question": "Organism with homodont dentition is",
+    "options": [
+      "Agama lizard",
+      "Rat",
+      "Cat",
+      "Man"
+    ],
+    "answer": "Agama lizard"
+  },
+  {
+    "question": "Functional unit of the kidney is",
+    "options": [
+      "Henle's loop",
+      "Bowman's capsule",
+      "Nephron",
+      "Glomerulus"
+    ],
+    "answer": "Nephron"
+  },
+  {
+    "question": "If secondary consumers decrease, producers will",
+    "options": [
+      "decrease",
+      "fluctuate",
+      "not be affected",
+      "reach climax population"
+    ],
+    "answer": "decrease"
+  },
+  {
+    "question": "Flower developing into fruit without fertilization is",
+    "options": [
+      "asexual reproduction",
+      "sexual reproduction",
+      "cloning",
+      "parthenocarpy"
+    ],
+    "answer": "parthenocarpy"
+  }
+];
